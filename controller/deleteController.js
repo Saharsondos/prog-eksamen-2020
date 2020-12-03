9aafa7b35db4c937564665082bd3516c1e67dd2f
Sahar Sondos <saharsondos@Sahars-Air.domain_not_set.invalid>
2020-12-03 12:10:19 +0100

    deletecontroller

---
+++ controller/deleteController.js
@@ -0,0 +1,12 @@
+var fs = require("fs")
+
+//function for deletecontroller - deletes "newUsers" json file
+
+/*function deleteController(req, res){
+    fs.unlink('newUsers.json', function (err) {
+        if (err) throw err;
+        console.log('File deleted!');
+      });
+}
+
+module.exports = deleteController;*/
