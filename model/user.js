17b6a582bf7bbb98354214fe50f5afb6b339bcd2
Sahar Sondos <saharsondos@Sahars-Air.domain_not_set.invalid>
2020-12-03 12:52:49 +0100

    user model klasse

--- model/user.js
+++ model/user.js
@@ -1,13 +1,14 @@
 //her defineres min modelklasse for users
 
 class User {
-    constructor(username, age, gender, preferredSex, email, password){
+    constructor(username, age, gender, preferredSex, email, password, interests){
         this.username = username;
         this.age = age;
         this.gender = gender;
         this.preferredSex = preferredSex;
         this.email = email;
-        this.password = password
+        this.password = password;
+        this.interests = interests
     }
 }
 
