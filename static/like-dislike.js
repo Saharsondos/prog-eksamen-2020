a52737e31c3537bf37947bb32d102824b8cfb6a0
Sahar Sondos <saharsondos@Sahars-Air.domain_not_set.invalid>
2020-12-10 09:13:53 +0100

    matching algoritme

--- static/like-dislike.js
+++ static/like-dislike.js
@@ -1,36 +1,39 @@
 
 var array = ["user1", "user2"];
 var myLike = []
-var minnieLike = ["username", "ib", "bob", "olaf"]
+var minnieLike = ["req.body.username", "ib", "bob", "olaf"]
 var mickeyLike = ["katja", "karla", "kaja"]
 var match = []
-//var minniLike = [req.body.username]
 
 var minnie = "minnie"
 var mickey = "mickey"
 
 //like button for Minnie
-$('.likeBtnMinnie').click(function like(){
+$('.likeBtnMinnie').click(function likeMinnie(){
     console.log("liked");
     myLike.push("minnie")
     console.log("i liked "+myLike)
 
+//matchin algorithm
+    if(myLike.includes('minnie') && minnieLike.includes("req.body.username")){
+    match.push(minnie)
+    alert('New match! you matched with ' + minnie)
+    localStorage.setItem("match", minnie)
+    location.reload()
+    }
+
 //removes the div when clicking the button
-    $(".harduserMinnie").remove();
+ $(".harduserMinnie").remove();   
     array.pop()
     if(array.length === 0){
     console.log("no users");
     $(".noUsers").show();
     }
 
-//matchin algorithm
-    if(myLike.includes('minnie') && minnieLike.includes('username')){
-    match.push(minnie)
-    alert('New match!')
-    }
 })
 
-//dislike button for Minnir
+
+//dislike button for Minni
 $('.dislikeBtnMinnie').click(function dislike(){
     console.log("disliked");
     $(".harduserMinnie").remove();
@@ -52,7 +55,13 @@ $('.likeBtnMickey').click(function like(){
     array.pop()
     if(array.length === 0){
     $(".noUsers").show();
-}
+    }
+    if(myLike.includes('mickey') && mickeyLike.includes('username')){
+        match.push(mickey)
+        alert('New match! you matched with' + mickey)
+        $(".matches").html(mickey)
+        }
+
 })
 
 //dislike button for mickey
@@ -66,10 +75,24 @@ $('.dislikeBtnMickey').click(function dislike(){
 }
 })
 
-//gemmer beskeden "no more users at the moment", viser først besked senere.
+//gemmer beskeden "no more users at the moment", vises først når begge hardcodede brugere er blevet liket eller disliket.
 $('.noUsers').hide()
 
+//viser match i frontend
+var usermatches = $(".matches").html(localStorage.getItem("match"))
 
-// get error "$ is not defined" when running server. 
-//module.exports.match = match;
+//slet match
+usermatches.click(function(){
+    localStorage.removeItem("match")
+    location.reload()
+})
 
+//tilføjer loginstatus i local storage
+$('.loginBtn').click(function(){
+    localStorage.setItem("logged in", $(".email").val())
+})
+
+//sletter loginstatus fra local storage
+$('.logoutBtn').click(function(){
+    localStorage.removeItem("logged in")
+})
